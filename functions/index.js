ad6dd5dd50012734edbb84bfa7fe0d308f5caf66
ficlel <victorleal190@gmail.com>
2020-07-30 11:10:28 +0200

    init

---
+++ functions/index.js
@@ -0,0 +1,130 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const nodemailer = require('nodemailer');
+const neo4j  =  require("neo4j-driver");
+
+const driver = neo4j.driver(
+   `bolt://3.248.198.172:7687`,
+   neo4j.auth.basic(`neo4j`, 'Fuckthisshit$1'),
+   { disableLosslessIntegers: true }
+);
+
+const cors = require('cors')({
+  origin: true
+});
+
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+// Your company name to include in the emails
+// TODO: Change this to your app or company name to customize the email sent.
+const APP_NAME = 'Biite';
+
+// [START sendWelcomeEmail]
+/**
+ * Sends a welcome email to new user.
+ */
+// [START onCreateTrigger]
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+// [END onCreateTrigger]
+  // [START eventAttributes]
+  const email = user.email; // The email of the user.
+  const displayName = user.displayName; // The display name of the user.
+  // [END eventAttributes]
+
+  return sendWelcomeEmail(email, displayName);
+});
+
+//[Start Register user in database]
+exports.registerUser =  functions.auth.user().onCreate((user)=>{
+  const email = user.email;
+  const name = user.displayName;
+  const userId = user.uid;
+  return addUser(name,email,userId);
+});
+
+//[End Register user in database]
+// [END sendWelcomeEmail]
+
+// [START sendByeEmail]
+/**
+ * Send an account deleted email confirmation to users who delete their accounts.
+ */
+// [START onDeleteTrigger]
+exports.sendByeEmail = functions.auth.user().onDelete((user) => {
+// [END onDeleteTrigger]
+  const email = user.email;
+  const displayName = user.displayName;
+
+  return sendGoodbyeEmail(email, displayName);
+});
+// [END sendByeEmail]
+
+// Sends a welcome email to the given user.
+async function sendWelcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email,
+  };
+
+  // The user subscribed to the newsletter.
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+  await mailTransport.sendMail(mailOptions);
+  console.log('New welcome email sent to:', email);
+  return null;
+}
+
+async function addUser(name,email,id){
+  const session = driver.session();
+  let response = {};
+  const setData = session.run(`create (p:person{id: $id,name: $name, email: $email, verified: true, type: 1}) return p`,{
+	id: id,
+	name: name,
+	email: email
+  }).then((result) => {
+	let response = result.records[0]._fields[0].properties;
+  }).catch(error => console.log(JSON.stringify(error)));
+  return response;
+}
+
+// Sends a goodbye email to the given user.
+async function sendGoodbyeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email,
+  };
+
+  // The user unsubscribed to the newsletter.
+  mailOptions.subject = `Bye!`;
+  mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
+  await mailTransport.sendMail(mailOptions);
+  console.log('Account deletion confirmation email sent to:', email);
+  return null;
+}
+
+exports.verify = functions.https.onRequest((req, res)=>{
+  let cors(req,res,()=>{
+	req.body.token	
+  });
+});
