a162dad5505a1fb319b08bd7bb78f0aebf3a6387
ficlel <victorleal190@gmail.com>
2020-08-03 22:13:25 +0200

    dont forget to commit

--- functions/index.js
+++ functions/index.js
@@ -23,43 +23,10 @@ const cors = require('cors')({
 });
 
 
+const app = admin.initializeApp();
 const Stripe = require("stripe");
 const stripe = Stripe("sk_test_51H4UivJ0n1vUgoZKN5bPsWmydn1JBhAjNxVc9tr66z8bt9p1OGL235Tw3ES5VOnFwmV8gbm7CswljnjoHvOxRF5C00qmuVjfJ6");
 
-// Configure the email transport using the default SMTP transport and a GMail account.
-// For Gmail, enable these:
-// 1. https://www.google.com/settings/security/lesssecureapps
-// 2. https://accounts.google.com/DisplayUnlockCaptcha
-// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
-// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword,
-  },
-});
-
-// Your company name to include in the emails
-// TODO: Change this to your app or company name to customize the email sent.
-const APP_NAME = 'Biite';
-
-// [START sendWelcomeEmail]
-/**
- * Sends a welcome email to new user.
- */
-// [START onCreateTrigger]
-exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
-// [END onCreateTrigger]
-  // [START eventAttributes]
-  const email = user.email; // The email of the user.
-  const displayName = user.displayName; // The display name of the user.
-  // [END eventAttributes]
-
-  return sendWelcomeEmail(email, displayName);
-});
 
 //[Start Register user in database]
 exports.registerUser =  functions.auth.user().onCreate((user)=>{
@@ -79,29 +46,6 @@ exports.registerUser =  functions.auth.user().onCreate((user)=>{
  * Send an account deleted email confirmation to users who delete their accounts.
  */
 // [START onDeleteTrigger]
-exports.sendByeEmail = functions.auth.user().onDelete((user) => {
-// [END onDeleteTrigger]
-  const email = user.email;
-  const displayName = user.displayName;
-  const phone = user.phoneNumber;
-  return sendGoodbyeEmail(email, displayName);
-});
-// [END sendByeEmail]
-
-// Sends a welcome email to the given user.
-async function sendWelcomeEmail(email, displayName) {
-  const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
-    to: email,
-  };
-
-  // The user subscribed to the newsletter.
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-  await mailTransport.sendMail(mailOptions);
-  console.log('New welcome email sent to:', email);
-  return null;
-}
 
 async function addUser(name,email,id,customer){
   const session = driver.session();
@@ -139,26 +83,11 @@ async function createStripeCustomer(name,email,id){
    return myCustomer;
 }
 
-// Sends a goodbye email to the given user.
-async function sendGoodbyeEmail(email, displayName) {
-  const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
-    to: email,
-  };
-
-  // The user unsubscribed to the newsletter.
-  mailOptions.subject = `Bye!`;
-  mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
-  await mailTransport.sendMail(mailOptions);
-  console.log('Account deletion confirmation email sent to:', email);
-  return null;
-}
 
 exports.verify = functions.https.onRequest((req, res)=>{
   return cors(req,res,()=>{
-    const app = admin.initializeApp();
     let uid = null;
-    admin.auth().verifyIdToken(idToken)
+    admin.auth().verifyIdToken(req.body.token)
   	.then(function(decodedToken) {
     	 uid = decodedToken.uid;
     	
@@ -168,4 +97,14 @@ exports.verify = functions.https.onRequest((req, res)=>{
 	});
       res.status(200).send(uid);
     });
+});
+
+exports.getDeliveries = functions.https.onRequest((req,rest)=>{
+  return cors(req,res,async()=>{
+	const database = admin.firestore();
+	const data = await db.collection('deliveries').get();
+	data.forEach((doc)=>{
+	  console.log(doc.id,' = ',doc.data());
+	});
+  })
 });
